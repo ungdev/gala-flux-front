03f5c9766d4730aef7b59580a6db3cddced3df6b
sun-tea <xthi.ngu@gmail.com>
2017-04-06 21:43:41 +0200

    Finished styling for alert component

--- src/components/log/Alert.jsx
+++ src/components/log/Alert.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Grid, Col } from 'react-flexbox-grid';
 
 require('../../styles/log/Alert.scss');
 
@@ -12,14 +12,26 @@ export default class Alert extends React.Component {
     render() {
         return (
             <Col xs={12} sm={6} md={6} lg={6} className="alert">
-                <div className="alert__team-name">
-                    
-                </div>
-                <div className="alert__content">
-                    Titre de l'alerte
-                    <span>Premières lignes de la description</span>
-                </div>
-                <div className="alert__action">
+                <div className="alert__container">
+                    <div className="alert__team-name__container">
+                        <div className="alert__team-name">
+                            TEAM PROPROPRO
+                        </div>
+                    </div>
+                    <div className="alert__content">
+                        <span className="alert__title">
+                            <p>
+                            Ceci est le titre de l'alerte qui indique entre autres son type
+                            </p>
+                        </span>
+                        <span className="alert__description">
+                            <p>
+                                Premières lignes de la description. Ceci est une description. Elle sera coupée sur l'alerte et lisible entièrement au survol.
+                            </p>
+                        </span>
+                    </div>
+                    <div className="alert__action">
+                    </div>
                 </div>
             </Col>
         );
