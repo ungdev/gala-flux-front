274ba432ce5393f44fcf80291a3db9ba6081f9ae
prudywsh <antoineprudhomme5@gmail.com>
2017-04-06 17:49:35 +0200

    some style + can create a new barrel type

--- src/components/pages/BarrelsPage.jsx
+++ src/components/pages/BarrelsPage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import { Row, Col } from 'react-flexbox-grid';
 import TypesList from '../barrelTypes/TypesList.jsx';
 
 export default class BarrelsPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <TypesList />
-            </div>
+            <Row className="hideContainer">
+                <Col xs={12} sm={6} className="hideContainer">
+                    <TypesList />
+                </Col>
+                <Col xs={12} sm={6} className="hideContainer">
+                    FUUUUTS
+                </Col>
+            </Row>
         );
     }
 
