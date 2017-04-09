32caf1810e1c19cb2f4c384df6810778b8e8ab61
prudywsh <antoineprudhomme5@gmail.com>
2017-04-09 22:51:55 +0200

    login part moved from home page to a login page + bug fixed in the barrels and barrel types stores

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import muiThemeable from 'material-ui/styles/muiThemeable';
-import RaisedButton from 'material-ui/RaisedButton';
-import AuthService from '../../services/AuthService';
-import NotificationActions from '../../actions/NotificationActions';
 require('../../styles/pages/HomePage.scss');
+
+import LoginPage from './LoginPage.jsx';
 
 class HomePage extends React.Component {
 
@@ -12,30 +11,13 @@ class HomePage extends React.Component {
         super(props);
 
         this._palette = props.muiTheme.palette;
-
-        this._login = this._login.bind(this);
-    }
-
-    /**
-     *  Redirect the user to the EtuUtt auth page
-     */
-    _login() {
-        NotificationActions.loading('Connexion depuis EtuUTT en cours..');
-        AuthService.authWithEtuUTT()
-        .then((data) => {
-            window.location = data.redirectUri;
-        })
-        .catch((error) => {
-            NotificationActions.error('Une erreur s\'est produite pendant l\'initialisation de la connexion via EtuUTT', error)
-        });
     }
 
     render() {
 
         return (
             <div className="container">
-                <h2 className="title">Flux</h2>
-                <RaisedButton label="Se connecter avec un compte UTT" primary={true} onTouchTap={this._login} />
+                <LoginPage />
             </div>
         );
     }
