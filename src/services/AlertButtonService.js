e3904fd16c064a4374bcb7bf0de49c5ff0e8f2b0
prudywsh <antoineprudhomme5@gmail.com>
2017-04-20 16:29:22 +0200

    can create an alert from a button

--- src/services/AlertButtonService.js
+++ src/services/AlertButtonService.js
@@ -9,6 +9,23 @@ class AlertButtonService extends BaseService {
         super('alertbutton');
     }
 
+    /**
+     * Send a request to create a new Alert from an AlertButton
+     *
+     * @param {string} id: the button id
+     * @param message
+     * @returns {Promise}
+     */
+    createAlert(id, message = "") {
+        return this._makeRequest({
+            method: 'post',
+            url: '/alertbutton/alert',
+            data: {
+                id,
+                message
+            }
+        });
+    }
 }
 
 export default new AlertButtonService();
