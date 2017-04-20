f15690eaf1f1b6800454a13771663341715e31ea
sun-tea <xthi.ngu@gmail.com>
2017-04-20 17:17:46 +0200

    Add dialog for assigned users on an alert

--- src/components/teams/dialogs/UpdateMemberDialog.jsx
+++ src/components/teams/dialogs/UpdateMemberDialog.jsx
@@ -51,8 +51,6 @@ export default class UpdateMemberDialog extends React.Component {
         });
     }
 
-
-
     /**
      * Called on field change
      *
--- src/services/AlertService.js
+++ src/services/AlertService.js
@@ -7,28 +7,7 @@ import AlertActions from '../actions/AlertActions';
 class AlertService {
 
     /**
-     * Send a new alert to the server
-     *
-     * @callback errorCallback
-     *
-     * @param {string} text
-     * @param {errorCallback} error
-     */
-    sendAlert(text, error) {
-        iosocket.request({
-            method: 'post',
-            url: '/alert/create',
-            data: {text}
-        }, (resData, jwres) => {
-            if (jwres.error) {
-                return error(jwres);
-            }
-            AlertActions.newAlert(text);
-        });
-    }
-
-    /**
-     * Make a webSocket request to get the alerts
+     * Make a request to get all the alerts
      *
      * @param {Array|null} filters
      * @return {Promise}
@@ -40,6 +19,24 @@ class AlertService {
                 data: {filters},
                 url: '/alert'
             }, (resData, jwres) => {
+                if(jwres.error) {
+                    return reject(new ApiError(jwres));
+                }
+                return resolve(resData);
+            });
+        });
+    }
+
+    /**
+      * Make a request to update an alert
+      *
+      * @param {String} alertId : the alert id
+      * @param {object} data : the attributes to update
+      * @return {Promise} A promise to end the update
+      */
+    updateAlert(alertId, data) {
+        return new Promise((resolve, reject) => {
+            iosocket.put('/alert/' + alertId, data, (resData, jwres) => {
                 if(jwres.error) {
                     return reject(new ApiError(jwres));
                 }
