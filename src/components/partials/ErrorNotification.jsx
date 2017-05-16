392720eb58f6dbdc58af46bbcca6dc4ab84a5ea5
Aurélien Labate <aurelien@labate.me>
2017-05-16 05:32:21 +0200

    Improve redirections and errors for EtuUTT auth

--- src/components/partials/ErrorNotification.jsx
+++ src/components/partials/ErrorNotification.jsx
@@ -120,7 +120,7 @@ export default class ErrorNotification extends React.Component {
     _closeDialog() {
         if(this.state.errorMessage.refresh) {
             AuthActions.logout();
-            location.reload();
+            location.href = '/';
         }
         this.setState({ errorMessage: null, count: this.state.count+1 });
     }
